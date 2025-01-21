af4d0b331ebe750487ceaccecad1c2d6c7a34d22
Rhodexa <69089201+Rhodexa@users.noreply.github.com>
2025-01-21 18:02:13 -0300

    Updated IOBUS functionality

--- VendettaKeys/iobus.h
+++ VendettaKeys/iobus.h
@@ -10,33 +10,43 @@
 */ /////////////////////////////////////////////////////////////////////////////
 
 // IOBus is an 8-bit bidirectional bus composed of:
-// PORTD[7-2] (pins 7 through 2) and PORTC[1-0] (pins A1 through A0)
-// So the mapping is quite neatly [7, 6, 5, 4, 3, 2, 1, 0] => [D7, D6, D5, D4, D3, D2, A1, A0]
-// (D stands for Arduino's Digital pin, and A is Arduino's Analog pin)
-// This can be modified to use PORTB if Ext. Interrupts on pins 2 and 3 are needed later on. For now, this is optimal.
+// PORTD[7-4] (pins 7 through 4) and PORTB[3-0] (pins 11 through 8)
+// So the mapping is quite neatly [7, 6, 5, 4, 3, 2, 1, 0] => [D11, D10, D9, D8, D7, D6, D5, D4]
+// This makes it very easy to use a simple SIL/header connector directly on the Arduino Pro Mini / Nano to plug the 8-bit bus.
+// It also leaves pins 0, 1, 2 and 3 free for serial and external interrupts if needed, which is neat.
 // Note: This code assumes OUTPUT = 1 and INPUT = 0, as defined by Arduino. Be careful
 
-void iobus_setMode(uint8_t mode){
+// Because the header in the Arduino Pro Mini / Nano exposes the bus with the nibbles physically swapped ([3210][7654]),
+// we can correct that in code for easier connections, you can disable this if you rather correct it in hardware for whatever reason.
+const bool IOBUS_ENABLE_SWAP_NIBBLES = true;
+
+// I hope the compiler will get this as a hint to use the SWAP NIBBLES instruction available in AVR... compilers are pretty clever (much more than you think!)
+inline uint8_t swapNibbles(uint8_t byte) {
+  return (byte >> 4) | (byte << 4);
+}
+
+void iobus_setMode(uint8_t mode){  
   if(mode == OUTPUT) {
     // TODO: Add a failsafe. Set addressing lines to a state where we know everyone's quiet (Hi-Z) to prevent "accidents" like bus fights, when switching to OUTPUT mode
-    DDRD |= 0b11111100;
-    DDRC |= 0b00000011;
+    DDRD |= 0b11110000;
+    DDRB |= 0b00001111;
   }
   else { // default to input, it is safer
-    DDRD &= 0b00000011;
-    DDRC &= 0b11111100;
+    DDRD &= 0b00001111;
+    DDRB &= 0b11110000;
   }
 }
 
 void iobus_setData(uint8_t data){
-  PORTD &= 0b00000011;
-  PORTC &= 0b11111100;
-  PORTD |= (0b11111100 & data);
-  PORTC |= (0b00000011 & data);
+  if(IOBUS_ENABLE_SWAP_NIBBLES) data = swapNibbles(data);
+  PORTD = (PORTD & 0b00001111) | (data & 0b11110000); 
+  PORTB = (PORTB & 0b11110000) | (data & 0b00001111);
 }
 
 uint8_t iobus_getData(){
-  return (PORTD & 0b11111100) | (PORTC & 0b00000011);
+  uint8_t data = (PIND & 0b11110000) | (PINB & 0b00001111);
+  if(IOBUS_ENABLE_SWAP_NIBBLES) data = swapNibbles(data);
+  return data;
 }
 
 #endif
--- VendettaKeys/keyboard.h
+++ VendettaKeys/keyboard.h
@@ -6,12 +6,12 @@
 /*
   An entire's sensitive key cycle might look something like this:
 
-             PRESSED   HELD       HALF_NOT_PRESSED
+             PRESSED   HELD       HALF_RELEASED
              v         v          v                               Timer also counts here, but takes no effect because the Key-ON event has already been issued
-              ____________________      NOT_PRESSED         ____  v  ___      __      v        and it can only be retriggred after a Key-OFF event, which only
+              ____________________      RELEASED         ____  v  ___      __      v        and it can only be retriggred after a Key-OFF event, which only
         _____/                    \____ v                __/    \___/   \    /  \    ___       occurs once the key is fully released, not half-released.
   ____/   ^                            \_________ _ ____/   ^         ^  \__/    \__/   \___
-  NOT_PRESSED  HALF_PRESSED                               KON   No-effect  ^   ^   ^      ^  
+  RELEASED  HALF_PRESSED                               KON   No-effect  ^   ^   ^      ^  
   .                                                                       KOFF KON KOFF   No Effect
              KEYON_EVENT              KEYOFF_EVENT                        
              v________________________ v                     ___________      ___
@@ -27,8 +27,8 @@
     KEYON VELOCITY SAMPLED      KEYOFF VELOCITY SAMPLED   
 
   All these stages are very relevant because
-  * A Note ON  event can only be triggered once the key traveled from NOT_PRESSED to PRESSED, but no other case.
-  * A Note OFF event can only be triggered once the key traveled from HELD/PRESSED to NOT_PRESSED, and no other case.
+  * A Note ON  event can only be triggered once the key traveled from RELEASED to PRESSED, but no other case.
+  * A Note OFF event can only be triggered once the key traveled from HELD/PRESSED to RELEASED, and no other case.
   * When a note enters the HALF_PRESSED range, a timer begins counting; This timer is used to compute the 'velocity' property of the note
 
   Note: Key OFF velocity isn't a standard thing in MIDI stuff, it may or may not work on some setups. I provide a switch for that feature.
@@ -52,7 +52,7 @@
   + + + + + + + + + + + + + + + + --> |___| KEY3 B / BA / State Bits
 
   BA -> State bits
-  * 00: NOT_PRESSED
+  * 00: RELEASED
   * 01: Half-press
   * 10: Shouldn't happen but it is still half-press
   * 11: Full press
@@ -77,12 +77,6 @@
 // Weirdly, it is the keyboard which queues notes for streaming, and not the MIDI engine which polls the keyboard!
 // This could be more elegant, but it doesn't have to! :D
 
-
-enum KeyStates{
-  NOT_PRESSED = 0b00,      // Key's bit pair = 00 = 0
-  FULLY_PRESSED = 0b11 // Key's bit pair = 11 = 3
-};
-
 struct Key {
   uint8_t state;
   bool keyon;
@@ -96,7 +90,7 @@ void keyboard_scanAndQueue(){
     // We need to capture two bytes: 
     // one for the lower part of the keyboard (first 4 keys of the current block) and one for the upper part (last 4 keys of the current block).
     // The following is just the ugly hardcoded nonsense that talks to the button matrix and gets one "block" worth of data,
-    // then it stores it in two separate variables _lo and _hi for later decoding and storing.
+    // then it stores it in two separate variables _lower and _upper for later decoding and storing.
     uint8_t block_select_mask = 1 << i;
     iobus_setMode(OUTPUT);
     iobus_setData(0);
@@ -105,7 +99,7 @@ void keyboard_scanAndQueue(){
     // strobe write signal for section A register
     iobus_setMode(INPUT);
     // enable keyboard read line
-    uint8_t column_data_lo = iobus_getData();
+    uint8_t column_data_lower = iobus_getData();
     // disable keyboard read line
     iobus_setMode(OUTPUT);
     iobus_setData(0);
@@ -114,7 +108,7 @@ void keyboard_scanAndQueue(){
     // strobe write signal for section B register
     iobus_setMode(INPUT);
     // enable keyboard read line
-    uint8_t column_data_hi = iobus_getData();
+    uint8_t column_data_upper = iobus_getData();
     // disable keyboard read line
 
     // Decode and store the 8 key states we just got:
@@ -125,35 +119,35 @@ void keyboard_scanAndQueue(){
     // * the keys in this block are 8 keys appart
     // * if the bit pair is 00 the key is no pressed; if it is 01 or 10, it is half-pressed; if it is 11 it is officially pressed
 
-    key[i     ].state = (column_data_lo     ) & 0b11); // Lower section   
-    key[i +  8].state = (column_data_lo >> 2) & 0b11);                    
-    key[i + 16].state = (column_data_lo >> 4) & 0b11); 
-    key[i + 24].state = (column_data_lo >> 6) & 0b11);  
-    key[i + 32].state = (column_data_hi     ) & 0b11); // Upper section
-    key[i + 40].state = (column_data_hi >> 2) & 0b11);
-    key[i + 48].state = (column_data_hi >> 4) & 0b11); 
-    key[i + 56].state = (column_data_hi >> 6) & 0b11); 
+    key[i     ].state = ((column_data_lower     ) & 0b11); // Lower section   
+    key[i +  8].state = ((column_data_lower >> 2) & 0b11);                    
+    key[i + 16].state = ((column_data_lower >> 4) & 0b11); 
+    key[i + 24].state = ((column_data_lower >> 6) & 0b11);  
+    key[i + 32].state = ((column_data_upper     ) & 0b11); // Upper section
+    key[i + 40].state = ((column_data_upper >> 2) & 0b11);
+    key[i + 48].state = ((column_data_upper >> 4) & 0b11); 
+    key[i + 56].state = ((column_data_upper >> 6) & 0b11); 
   }
 
   // Step 2 — Process the states to find events and velocity information, then queue any event
   midi_note_queue_index = 0; // flush the midi queue;
   for (uint8_t i = 0; i < N_KEYS; i++) {
     // Key logic
-    if(key[i].state == KeyStates::FULLY_PRESSED) {
+    if(key[i].state == 0b11) { // Pressed state
       if (key[i].keyon == false){
         key[i].keyon = true;
         midi_queueKey(true, i, key[i].timer);
       }
       key[i].timer = 0; // Order matters! Only reset the timer after checking for events, this gives the event the chance to queue the timer's value before it gets reset
     }
-    else if(key[i].state == KeyStates::NOT_PRESSED) {
+    else if(key[i].state == 0b00) { // Released state
       if (key[i].keyon == true){
         key[i].keyon = false;
         midi_queueKey(true, i, key[i].timer);
       }
       key[i].timer = 0;
     }
-    else { // If not NOT_PRESSED nor FULLY_PRESSED we must be in a HALF_PRESSED state! — The timer always counts when HALF_PRESS'ed, and gets reset in any other case
+    else { // If not RELEASED nor PRESSED we must be in a HALF_PRESSED state! — The timer always counts when HALF_PRESS'ed, and gets reset in any other case
       key[i].timer++;
     }
   }
